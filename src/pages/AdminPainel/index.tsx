8d8803f4ba129aa0fafd6db7cbb0919d4bfaa177
Luiz Paulo Pilegi de Almeida <luiz.pilegi@condoconta.com.br>
2022-06-29 23:28:57 -0300

    creating new menu

--- src/pages/AdminPainel/index.tsx
+++ src/pages/AdminPainel/index.tsx
@@ -1,29 +1,86 @@
 import React from "react";
-import { Link, Route, Routes } from "react-router-dom";
+import { Route, Routes, useNavigate } from "react-router-dom";
 import NavBar from "../../components/NavBar";
 import CondominiumHome from "../CondominiumHome";
 import Payments from "../Payments";
-import { Menu, Container, Body, Children } from "./styles";
-
+import { TbHome } from "react-icons/tb";
+import { RiNewspaperLine } from "react-icons/ri";
+import { MdOutlinePayments, MdOutlineManageAccounts, MdSchedule } from "react-icons/md";
+import { GiReceiveMoney } from "react-icons/gi";
+import { AiOutlineDownload } from "react-icons/ai";
+import {
+  Menu,
+  Container,
+  Body,
+  Children,
+  ChangeCondo,
+  TextChangeCondo,
+  CondoText,
+  CondoName,
+  CondoInfo,
+  ItemMenu,
+  ItemMenuName
+} from "./styles";
 export interface IAdminPainel {
   children?: React.ReactNode;
 }
 
 const AdminPainel: React.FC<IAdminPainel> = ({ children }) => {
+  const navigate = useNavigate();
+
+  function handleChangeCondo() {
+    navigate("/condominios");
+  }
+
   return (
     <Container>
       <NavBar />
       <Body>
         <Menu>
-          <h1>menu</h1>
-          <br/><br/>
-          <Link to={'/condominios'}>condominiums</Link>
-          <br/><br/>
-          <Link to={'/condominios/:cnpj/home'}>dashboard</Link>
-          <br/><br/>
-          <Link to={'/condominios/:cnpj/payments'}>payments</Link>
-          <br/><br/>
-          <Link to={'/login'}>login</Link>
+          <CondoInfo>
+            <CondoName>Condomínio ABC</CondoName>
+            <CondoText>CNPJ: 76.224.335/0001-55</CondoText>
+            <CondoText>Síndico: MARCIA TORQUATTO</CondoText>
+          </CondoInfo>
+          <ChangeCondo>
+            <TextChangeCondo onClick={handleChangeCondo}>
+              RETORNAR A LISTA DE CONDOMÍNIOS
+            </TextChangeCondo>
+          </ChangeCondo>
+          <ItemMenu>
+            <TbHome size={20} />
+            <ItemMenuName>Home</ItemMenuName>
+          </ItemMenu>
+          <ItemMenu>
+            <RiNewspaperLine size={20} />
+            <ItemMenuName>Extrato</ItemMenuName>
+          </ItemMenu>
+          <ItemMenu>
+            <RiNewspaperLine size={20} />
+            <ItemMenuName>Demonstrativo Financeiro</ItemMenuName>
+          </ItemMenu>
+          <ItemMenu>
+            <MdOutlinePayments size={20} />
+            <ItemMenuName>Pagamentos</ItemMenuName>
+          </ItemMenu>
+          <ItemMenu>
+            <GiReceiveMoney size={20} />
+            <ItemMenuName>Investimentos</ItemMenuName>
+          </ItemMenu>
+          <ItemMenu>
+            <MdOutlineManageAccounts size={20} />
+            <ItemMenuName>Gerenciar condôminos</ItemMenuName>
+          </ItemMenu>
+          <ItemMenu>
+            <MdSchedule size={20} />
+            <ItemMenuName>Meus agendamentos</ItemMenuName>
+          </ItemMenu>
+          <ItemMenu>
+            <AiOutlineDownload size={20} />
+            <ItemMenuName>Depositar</ItemMenuName>
+          </ItemMenu>
+          {/* <Link to={"/condominios/:cnpj/payments"}>payments</Link>
+          <Link to={"/condominios/:cnpj/home"}>dashboard</Link> */}
         </Menu>
         <Children>
           <Routes>
--- src/pages/AdminPainel/styles.ts
+++ src/pages/AdminPainel/styles.ts
@@ -15,10 +15,58 @@ export const Body = styled.div`
 export const Menu = styled.div`
   width: 100%;
   max-width: 250px;
-  background-color: #FFFFFF;
+  background-color: rgb(249, 249, 249);;
 `;
 
 export const Children = styled.div`
   width: 100%;
-  background-color: blue;
+`;
+
+export const ChangeCondo = styled.div`
+  display: flex;
+  justify-content: center;
+  padding-bottom: 16px;
+  font-size: 10px;
+`;
+
+export const TextChangeCondo = styled.p`
+  color: rgb(56, 109, 130);
+  cursor: pointer;
+  font-weight: bold;
+`;
+
+export const CondoInfo = styled.div`
+  background-color: rgb(255, 255, 255);
+  margin: 16px 16px;
+  padding: 16px;
+`;
+
+export const CondoName = styled.p`
+  font-size: 11px;
+  font-weight: bold;
+`;
+
+export const CondoText = styled.p`
+  font-size: 11px;
+  margin-top: 6px;
+`;
+
+export const ItemMenu = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 6px;
+  padding-left: 16px;
+  margin-top: 3px;
+  cursor: pointer;
+  
+  &:hover {
+    background-color: #e6ebed;
+    border-right: solid 3px;
+    font-weight: bold;
+  }
+`;
+
+export const ItemMenuName = styled.div`
+  font-size: 12px;
+  margin-left: 16px;
 `;
